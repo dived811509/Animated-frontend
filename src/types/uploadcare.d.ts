4fed4488f1e593f5af0b31f5dd8e515f2f3cd9fc
Dived <mishradived@gmail.com>
2026-02-17 04:52:58 +0530

    Fix JSX types for Uploadcare custom elements

--- src/types/uploadcare.d.ts
+++ src/types/uploadcare.d.ts
@@ -1,16 +1,22 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    "lr-config": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    >;
-    "lr-file-uploader-regular": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    >;
-    "lr-upload-ctx-provider": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    >;
+import React from "react";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "lr-config": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+      "lr-file-uploader-regular": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+      "lr-upload-ctx-provider": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
   }
 }
+
+export {};
